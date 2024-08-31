6eb404a29d6fdcab7018c35f4a017ab9ab7d8820
Noodle Network <130073055+Noodle-Network@users.noreply.github.com>
2024-08-31 20:55:16 +0200

    cat beeep meow boop

--- js/my.js
+++ js/my.js
@@ -1,20 +1,14 @@
 
-function showCat() { 
+function playCat() 
+{ 
   var cat = document.getElementById("cat");
-  if (cat.style.display === "none") {
+  
+  if (cat.style.display === "none")
+  {
     cat.style.display = "block";
-    document.body.scrollIntoView(false);   
-    // focus(cat);
     cat.play();
-
-  } else {
-    cat.style.display = "none";
-  }                        
+    document.body.scrollIntoView(false);
+  } 
+  else cat.style.display = "none";               
 } 
-
-
-
-function playCat() {
-  var cat = document.getElementById("catVideo");  
-  cat.play();
-}                        
+         
