a915d19a2ed9ce48ea9bbf134b8c525fbcf5cc7b
Noodle Network <130073055+Noodle-Network@users.noreply.github.com>
2024-08-31 14:36:43 +0200

    hf

--- js/my.js
+++ js/my.js
@@ -1,13 +1,21 @@
 
-  function showCat() { 
-    var cat = document.getElementById("cat");
-    if (cat.style.display === "none") {
-      cat.style.display = "block";
-      document.body.scrollIntoView(false);
-    } else {
-      cat.style.display = "none";
-    }                        
-  } 
+function showCat() { 
+  var cat = document.getElementById("cat");
+  if (cat.style.display === "none") {
+    cat.style.display = "block";
+    document.body.scrollIntoView(false);      
+  } else {
+    cat.style.display = "none";
+  }                        
+} 
 
 
 
+function playCat() {
+  var cat = document.getElementById("catVideo");
+  // cat.remove();
+  cat.load();
+// Add .vid to body
+// document.body.appendChild(cat);
+  cat.play();
+}                        
